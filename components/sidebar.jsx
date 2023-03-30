f6fed496668d26d5d4149b377e3aad43812be456
Yeony99 <ny.yeony.kim@gmail.com>
2023-03-30 14:04:52 +0900

    style: add key on sidebar

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,35 +1,54 @@
-import { useState } from "react";
 import Link from "next/link";
 import styles from "../styles/sidebar.module.css";
-import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from "react-pro-sidebar";
+import {
+  Sidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  useProSidebar,
+} from "react-pro-sidebar";
 
 const SideBar = ({ menuItems }) => {
 
   const { collapseSidebar } = useProSidebar();
 
   return (
-    <Sidebar>
-      <Menu>
-        {menuItems.map((menuItem) => (
-          <>
-            {
-                !menuItem.submenu &&
-                <MenuItem component={<Link href={menuItem.link} />}>{menuItem.name}</MenuItem>
-            }
-            {
-                menuItem.submenu && (
-                    <SubMenu label={menuItem.name}>
-                        {
-                            menuItem.submenu.map(submenuItem => (
-                                <MenuItem component={<Link href={submenuItem.link} />}>{submenuItem.name}</MenuItem>
-                            ))
-                        }
-                    </SubMenu>
-                )
-            }
-          </>
+    <Sidebar key={"sidebar"}>
+      <Menu key={"menu"}>
+        {menuItems.map((menuItem, i) => (
+          <div key={i}>
+            {menuItem.submenu && (
+              <SubMenu key={i + "side"} label={menuItem.name}>
+                {menuItem.submenu.map((submenuItem, j) => (
+                  <MenuItem
+                    key={j + "sub"}
+                    component={
+                      <Link key={j + "sub-link"} href={submenuItem.link} />
+                    }
+                  >
+                    {submenuItem.name}
+                  </MenuItem>
+                ))}
+              </SubMenu>
+            )}
+            {!menuItem.submenu && (
+              <MenuItem
+                key={i + "menu"}
+                component={<Link key={i + "link"} href={menuItem.link} />}
+              >
+                {menuItem.name}
+              </MenuItem>
+            )}
+          </div>
         ))}
-        <div onClick={() => collapseSidebar()} style={{float: 'right'}}><button>X</button></div>
+
+        <div
+          key="collapseButton"
+          onClick={() => collapseSidebar()}
+          style={{ float: "right" }}
+        >
+          <button>X</button>
+        </div>
       </Menu>
     </Sidebar>
   );
