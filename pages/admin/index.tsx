0c6f1fb22e1a8e3093d06c38c43f87503b4d8ceb
Yeony99 <ny.yeony.kim@gmail.com>
2023-05-03 17:28:25 +0900

    feat: add role on hasPermission

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -2,7 +2,7 @@ import { GetServerSidePropsContext } from "next";
 import { getServerSideProps as baseGetServerSideProps } from "./utils";
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const baseProps = await baseGetServerSideProps();
+  const baseProps = await baseGetServerSideProps("USER"); // @TODO redux에 저장된 user 정보 받아 넣기
   if (baseProps.redirect) {
     return { ...baseProps };
   }
--- pages/admin/utils.tsx
+++ pages/admin/utils.tsx
@@ -1,16 +1,15 @@
 // import { GetServerSidePropsContext } from "next";
 import checkPermission from "../../lib/auth/permission";
 
-export const getServerSideProps = () => {
-  const userRole = "USER";
-  const hasPermission = checkPermission(userRole);
+export const getServerSideProps = (role: string) => {
+  const hasPermission = checkPermission(role);
 
   if (!hasPermission) {
     return { redirect: { permanent: false, destination: "/404" } };
   }
   return {
     props: {
-      userRole,
+      role,
       // ...context
     },
   };
